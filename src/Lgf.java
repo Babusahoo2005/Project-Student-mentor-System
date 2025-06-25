549190e59ea8dd277263ecbf03860b2ad0b10ae0
Babusahoo2005 <baburamsahoo2005@gmail.com>
2025-06-25 12:30:04 +0530

    Add files via upload

---
+++ src/Lgf.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+class Lgf {
+
+    static void pack() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setLocationRelativeTo(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setDefaultCloseOperation(int EXIT_ON_CLOSE) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/mentorsystem/Mentorsystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mentorsystem;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Mentorsystem {
+
+    /**
+     * @param args the command line arguments
+     */Z
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
